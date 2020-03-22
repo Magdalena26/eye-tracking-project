9722f0c2d0af16f240b02d55a1fd87b32c2fcf6e
Patryk Sladek <sladzio995@gmail.com>
2020-03-22 16:44:57 +0100

    *Created eye tracking

---
+++ Eye Tracking/Assets/EyeTracking/Scripts/EyeTracker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARFace))]
+public class EyeTracker : MonoBehaviour
+{
+    [SerializeField] private GameObject _eyePrefab;
+
+    private GameObject _leftEye;
+    private GameObject _rightEye;
+    
+    private ARFace _arFace;
+
+    private void Awake()
+    {
+        _arFace = GetComponent<ARFace>();
+    }
+
+    private void OnEnable()
+    {
+        ARFaceManager arFaceManager = FindObjectOfType<ARFaceManager>();
+        if (arFaceManager != null && arFaceManager.subsystem != null && arFaceManager.subsystem.SubsystemDescriptor.supportsEyeTracking)
+        {
+            _arFace.updated += OnFaceUpdated;
+        }
+        else
+        {
+            Debug.Log("Eye tracking not supported");
+        }
+    }
+
+    private void OnDisable()
+    {
+        _arFace.updated -= OnFaceUpdated;
+        SetEyeVisibility(false);
+    }
+
+    private void OnFaceUpdated(ARFaceUpdatedEventArgs obj)
+    {
+        if (_arFace.leftEye != null && _leftEye == null)
+        {
+            _leftEye = Instantiate(_eyePrefab, _arFace.leftEye);
+            _leftEye.SetActive(false);
+        }
+        
+        if (_arFace.rightEye != null && _rightEye == null)
+        {
+            _rightEye = Instantiate(_eyePrefab, _arFace.rightEye);
+            _rightEye.SetActive(false);
+        }
+
+        bool isVisible = _arFace.trackingState == TrackingState.Tracking && ARSession.state > ARSessionState.Ready;
+        SetEyeVisibility(isVisible);
+    }
+
+    private void SetEyeVisibility(bool isVisible)
+    {
+        if (_rightEye != null) _rightEye.SetActive(isVisible);
+        if (_leftEye != null) _leftEye.SetActive(isVisible);
+    }
+}
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This repository contains Unity3d mobile application enabling eye tracking using 
 The application tracks user's eyes using front camera and displays some 3D models.
 
 ## Tools
-- Unity
+- Unity (2019.2.21f1)
 - AR Foundation
